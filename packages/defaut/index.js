4277f850e506253fa4865d450b5208d031e3c957
JiangWeixian <jiangweixian1994@gmail.com>
2021-07-21 10:47:54 +0800

    :sparkles:  add prettier back

--- packages/defaut/index.js
+++ packages/defaut/index.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ["@aiou/eslint-config-react", "plugin:prettier/recommended"],
+  rules: {
+    "prettier/prettier": ["error", {"printWidth": 100,
+    "semi": false,
+    "singleQuote": true,
+    "useTabs": false,
+    "tabWidth": 2,
+    "trailingComma": "all",}]
+  }
 };
