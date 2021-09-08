bd817c537158ad4f2286d780c800e24c187bdfe3
jiangWeixian <jiangweixian1994@gmail.com>
2021-09-08 22:54:53 +0800

    :zap:  error to warn

--- packages/analyze/src/configs/all.ts
+++ packages/analyze/src/configs/all.ts
@@ -2,6 +2,6 @@ export = {
   parser: '@typescript-eslint/parser',
   plugins: ['@aiou/analyze'],
   rules: {
-    '@aiou/analyze/comment-density': ['error', { min: 10 }],
+    '@aiou/analyze/comment-density': ['warn', { min: 10 }],
   },
 }
