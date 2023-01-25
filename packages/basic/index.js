0fb3f156e3c473eb2b668ebbbeba2cc090ad5b03
JiangWeixian <jiangweixian1994@gmail.com>
2023-01-25 21:53:46 +0800

    :alembic:  sort imports

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -268,5 +268,15 @@ module.exports = {
 
     'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
     'eslint-comments/disable-enable-pair': 'off',
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
+      },
+    ],
   },
 }
