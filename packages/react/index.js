f1b708f1ef085f8f9e0934b81865be025a1704b5
JiangWeixian <jiangweixian1994@gmail.com>
2023-04-16 13:23:29 +0800

    :sparkles:  update jsx style related react rules

--- packages/react/index.js
+++ packages/react/index.js
@@ -23,5 +23,23 @@ module.exports = {
     'react/no-unescaped-entities': 'off',
     'react/no-unknown-property': 'off',
     'react/jsx-boolean-value': ['error', 'always'],
+    // Enforce new line when declare jsx element e.g. const element = (\newline<div
+    'react/jsx-wrap-multilines': [
+      'warn',
+      {
+        declaration: 'parens-new-line',
+        assignment: 'parens-new-line',
+        return: 'parens-new-line',
+        arrow: 'parens-new-line',
+        condition: 'parens-new-line',
+        logical: 'ignore',
+        prop: 'parens-new-line',
+      },
+    ],
+    'react/jsx-closing-tag-location': 'error',
+    // Enforce disallow </ div>
+    'react/jsx-space-before-closing': ['error', 'always'],
+    'react/jsx-max-props-per-line': ['warn', { maximum: 2 }],
+    'react/jsx-indent': ['warn', 2],
   },
 }
