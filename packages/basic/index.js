5e287c3362a5513c1511dc6198ba71355612bdac
JW <jiangweixian1994@gmail.com>
2023-06-21 16:00:47 +0800

    :sparkles:  lint markdown (#65)

--- packages/basic/index.js
+++ packages/basic/index.js
@@ -15,6 +15,7 @@ module.exports = {
     'plugin:eslint-comments/recommended',
     'plugin:jsonc/recommended-with-jsonc',
     'plugin:yml/standard',
+    'plugin:markdown/recommended',
     isTailwindcss ? 'plugin:tailwindcss/recommended' : undefined,
   ]),
   plugins: ['html', 'unicorn', 'unused-imports', 'simple-import-sort', 'import-newlines'],
@@ -148,6 +149,33 @@ module.exports = {
         // related: https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/docs/TROUBLESHOOTING.md#edits-always-lead-to-full-reload
         // export anonymous function: ReactRefresh failed
         'import/no-anonymous-default-export': 'warn',
+      },
+    },
+    {
+      // Enable the Markdown processor for all .md files.
+      files: ['**/*.md'],
+      processor: 'markdown/markdown',
+    },
+    {
+      // Code blocks in markdown file
+      files: ['**/*.md/*.*'],
+      rules: {
+        'react/display-name': 'off',
+        '@typescript-eslint/no-redeclare': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/consistent-type-imports': 'off',
+        '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-require-imports': 'off',
+        'import/no-unresolved': 'off',
+        'no-alert': 'off',
+        'no-console': 'off',
+        'no-restricted-imports': 'off',
+        'no-undef': 'off',
+        'no-unused-expressions': 'off',
+        'no-unused-vars': 'off',
       },
     },
   ],
