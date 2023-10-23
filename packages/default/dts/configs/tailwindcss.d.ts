0b5bea130c0fc6d4ac5178fbf53705fbc435fd6a
JW <jiangweixian1994@gmail.com>
2023-10-23 22:50:16 +0800

    :bug:  glob patterns

---
+++ packages/default/dts/configs/tailwindcss.d.ts
@@ -0,0 +1,2 @@
+import type { FlatESLintConfigItem } from 'eslint-define-config';
+export declare const tailwindcss: () => FlatESLintConfigItem[];
---
+++ packages/default/dts/index.d.ts
@@ -0,0 +1,2 @@
+import type { FlatESLintConfigItem } from 'eslint-define-config';
+export declare const aiou: (config?: FlatESLintConfigItem | FlatESLintConfigItem[]) => FlatESLintConfigItem[];
