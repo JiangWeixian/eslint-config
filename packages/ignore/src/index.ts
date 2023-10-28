7812ed659469c273796eb2e3901ee0bf06c7a50d
JW <jiangweixian1994@gmail.com>
2023-10-28 21:19:08 +0800

    :recycle:  add rust releated ignore

--- packages/ignore/src/index.ts
+++ packages/ignore/src/index.ts
@@ -1,17 +1,25 @@
+import fs from 'node:fs'
+import path from 'node:path'
+
 // eslint-disable-next-line import/no-extraneous-dependencies
 import axios from 'axios'
-import fs from 'fs'
-import path from 'path'
 
 import defaultIgnorePatterns from './default'
 
 const main = async () => {
-  const result: string = await axios
-    .get('https://www.toptal.com/developers/gitignore/api/node')
-    .then(res => res.data)
-  const nodePatterns = result.split('\n').filter((line) => {
-    return !line.startsWith('#') && !!line
-  })
+  const [node, rust] = await Promise.all<string>([
+    await axios
+      .get('https://www.toptal.com/developers/gitignore/api/node')
+      .then(res => res.data),
+    await axios
+      .get('https://www.toptal.com/developers/gitignore/api/rust')
+      .then(res => res.data),
+  ])
+  const nodePatterns = node.split('\n')
+    .concat(rust.split('\n'))
+    .filter((line) => {
+      return !line.startsWith('#') && !!line
+    })
   const patterns = nodePatterns.concat(defaultIgnorePatterns).map(line => line.startsWith('**') ? `"${line}"` : `"**/${line}"`)
   const exportIgnorePatterns = `
     module.exports = [${patterns}]
