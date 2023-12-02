05ee4d06cc29657045a478a6e8fcf41841eed335
JW <jiangweixian1994@gmail.com>
2023-12-02 14:19:33 +0800

    :sparkles:  support options.ssr (#99)

--- packages/default/src/configs/react.ts
+++ packages/default/src/configs/react.ts
@@ -2,6 +2,7 @@ import pluginReact from 'eslint-plugin-react'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
 import pluginReactRefresh from 'eslint-plugin-react-refresh'
 import pluginSSRFriendly from 'eslint-plugin-ssr-friendly'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { mapValues } from 'lodash-es'
 
 import { GLOB_JSX, GLOB_TSX } from '../globs'
@@ -27,20 +28,45 @@ export const react = () => {
       plugins: {
         react: pluginReact,
         'react-hooks': pluginReactHooks,
-        'ssr-friendly': pluginSSRFriendly,
         'react-refresh': pluginReactRefresh,
       },
       rules: {
         ...(pluginReact.configs.recommended.rules as Rules),
         ...(pluginReact.configs['jsx-runtime'].rules as Rules),
         ...(pluginReactHooks.configs.recommended.rules as Rules),
-        ...(pluginSSRFriendly.configs.recommended.rules as Rules),
         'react/prop-types': 'off',
         'react/no-unescaped-entities': 'off',
         'react/no-unknown-property': 'off',
         'react/jsx-boolean-value': ['error', 'always'],
         // https://github.com/ArnaudBarre/eslint-plugin-react-refresh
         'react-refresh/only-export-components': 'warn',
+      },
+    },
+  ]
+  return config
+}
+
+export const ssrReact = () => {
+  const config: FlatESLintConfig[] = [
+    {
+      files: [GLOB_JSX, GLOB_TSX],
+      languageOptions: {
+        parserOptions: {
+          ...pluginReact.configs.recommended.parserOptions,
+          ...pluginReact.configs['jsx-runtime'].parserOptions,
+        },
+      },
+      settings: {
+        react: {
+          // 'detect' will throw warn on monorepo
+          version: '18.0',
+        },
+      },
+      plugins: {
+        'ssr-friendly': pluginSSRFriendly,
+      },
+      rules: {
+        ...(pluginSSRFriendly.configs.recommended.rules as Rules),
       },
     },
     {
