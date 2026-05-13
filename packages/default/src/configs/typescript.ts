7d47edc36f940d17b614809cdcca0d8cc6fb0d03
JiangWeixian <jiangweixian1994@gmail.com>
2026-05-13 08:48:51 +0800

    fix(typescript): remove deprecated/removed rules, use built-in no-loss-of-precision

--- packages/default/src/configs/typescript.ts
+++ packages/default/src/configs/typescript.ts
@@ -49,10 +49,7 @@ export const typescript = () => {
         ],
         // Limit `interface` define object types, users could override with *.d.ts declare
         '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
-        '@typescript-eslint/prefer-ts-expect-error': 'warn',
         '@typescript-eslint/default-param-last': 'error',
-        // Sort type S = A | B
-        '@typescript-eslint/sort-type-constituents': 'error',
         '@typescript-eslint/unbound-method': 'off',
         '@typescript-eslint/prefer-for-of': 'error',
         // When .ts files compiled to .mjs, will throw require is not found
@@ -96,8 +93,7 @@ export const typescript = () => {
           'error',
           { functions: false, classes: false, variables: true },
         ],
-        'no-loss-of-precision': 'off',
-        '@typescript-eslint/no-loss-of-precision': 'error',
+        'no-loss-of-precision': 'error',
         'no-unused-expressions': 'off',
         '@typescript-eslint/no-unused-expressions': ['error', {
           allowShortCircuit: true,
@@ -106,19 +102,14 @@ export const typescript = () => {
         }],
 
         // off
-        '@typescript-eslint/camelcase': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-member-accessibility': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-parameter-properties': 'off',
-        '@typescript-eslint/no-empty-interface': 'off',
-        '@typescript-eslint/ban-ts-ignore': 'off',
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/ban-types': 'off',
         '@typescript-eslint/no-namespace': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
+
         // https://www.npmjs.com/package/eslint-plugin-unused-imports
         '@typescript-eslint/no-unused-vars': 'off',
         'no-void': ['error', { allowAsStatement: true }],
